6d060331a80b460172bfc19f7e0c28cef61fc3de
zainabtar <zainab.tariq@iu-study.org>
2026-03-21 13:52:42 +0400

    habit.py

---
+++ habit.py
@@ -0,0 +1,7 @@
+
+# Creating habit object
+class Habit:
+    def __init__(self, name, description, periodicity):
+        self.name = name
+        self.description = description
+        self.periodicity = periodicity
